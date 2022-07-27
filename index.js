72b8039f698e5c3604b3fc1764f1eb906242d4ce
kmart134 <ksmgps@hotmail.com>
2022-07-27 17:58:06 -0400

    added first set of iquirer prompts and switch cases

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
 const inquirer = require("inquirer");
 const logo = require("asciiart-logo");
+
+
+// ascciartlogo
+
+
+//begin function with inquire prompts
+function init (){
+    inquirer.prompt([
+      {
+        type:"list",
+        name:"initialChoice",
+        message:"What would you like to do?",
+        choices:[
+            "View all Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View all Roles",
+            "Add Role",
+            "View all Departments",
+            "Add Departmet"
+        ],
+      }  
+    ])
+
+    //send to switch case according to user choice
+
+    .then((choice) => {
+        switch(choice.initialChoice) {
+        case "View all Employeesr":
+            viewEmployees();
+            break;
+        case "Add Employee":
+            addEmployee();
+            break;
+        case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+        case "View all Roles":
+            viewRoles();
+            break;
+        case "Add Role":
+            addRole();
+            break;
+        case "View all Departments":
+            viewDepartmets();
+            break;
+        case "Add Departmet":
+            addDepartment();
+            break;
+        //no default
+        }
+    })
+
+}
+init();
